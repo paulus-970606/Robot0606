2ceb3d441ba5a3a3daad33bd1e3af1d48955b305
paulus-970606 <77953741+paulus-970606@users.noreply.github.com>
2021-01-27 16:09:05 +0800

    Add files via upload

---
+++ day3/1.hits.py
@@ -0,0 +1,17 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+
+while True:
+    hits = mc.events.pollBlockHits()
+
+    if len(hits) > 0:
+        hit = hits[0]
+        x,y,z=hit.pos.x, hit.pos.y, hit.pos.z
+        block=mc.getBlock(x,y,z)
+        mc.postToChat("恭喜你獵到了"+str(block))
+        
+        #劍
+        
+        
+        
---
+++ day3/10.def 函式.py
@@ -0,0 +1,40 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+x,y,z=mc.player.getTilePos()
+
+
+def plantTree(x,y,z):
+    mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,161)
+    mc.setBlocks(x,y,z,x,y+4,z,17)
+
+plantTree(x,y,z)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+        
+        
+        
